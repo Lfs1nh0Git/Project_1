7adcf27e5289737efb6c7a930e651cfbd538ee83
Lfs1nh0Git <lufepasousa@gmail.com>
2025-11-21 16:33:18 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function login() {
+    var nomeId = document.getElementById("nomeId")
+    var nome = nomeId.value
+    console.log("Nome: " + nome)
+    var senha = document.getElementById("senha")
+    var senhaa = senha.value
+    console.log("Senha: " + senhaa)
+}
